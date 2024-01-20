5dad5159135884bd3a120e76f6a921a90db8ed47
علش <153824248+nesrx@users.noreply.github.com>
2024-01-20 17:54:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ db = dt("data.sqlite", 'fuck')
 if not db.get("checker"):
   db.set('checker', None)
 if not db.get("admin_list"):
-  db.set('admin_list', [id, 5871764800,6186856384])
+  db.set('admin_list', [id, 6324688689,6324688689])
 if not db.get('ban_list'):
   db.set('ban_list', [])
 if not db.get('sessions'):
